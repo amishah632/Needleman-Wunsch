f7f1ec5039f11974e92b65bbfc09fad5467a4c4a
Ami Shah <shahami.406@gmail.com>
2019-11-11 17:29:52 +0100

    Create config.py
    Contains all required attributes

---
+++ config.py
@@ -0,0 +1,5 @@
+GAP_PENALTY = -2  # penalty points for adding GAP
+SAME = 5  # points for same(identical) symbols
+DIFF = -5  # points for different symbols
+MAX_SEQ_LENGTH = 10  # Maximum length of sequence
+MAX_NUMBER_PATHS = 8  # Maximum number of possoble alignments(paths)
