a9e9e5ff75ac16d82bc59cfe394a1d33aacc557e
Ami Shah <shahami.406@gmail.com>
2019-11-13 08:50:54 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ GAP_PENALTY = -2  # penalty points for adding GAP
 SAME = 5  # points for same(identical) symbols
 DIFF = -5  # points for different symbols
 MAX_SEQ_LENGTH = 10  # Maximum length of sequence
-MAX_NUMBER_PATHS = 8  # Maximum number of possoble alignments(paths)
+MAX_NUMBER_PATHS = 8  # Maximum number of possible alignments(paths)
