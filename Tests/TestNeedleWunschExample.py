8e8ea0ab59310b41c90bd209739f968526085a7b
Ami Shah <shahami.406@gmail.com>
2019-11-24 00:08:13 +0100

    Update TestNeedleWunschExample.py
    i made changes in NeedlemanWunsch.py file. so, unit testing of code is also changed.

--- Tests/TestNeedleWunschExample.py
+++ Tests/TestNeedleWunschExample.py
@@ -1,59 +1,37 @@
 import unittest
-from Alignment import *
 from NeedlemanWunsch import *
 
 class TestNeedleAlignment(unittest.TestCase):
+    def testExample1(self):
+        if MAX_NUMBER_PATHS == 2 and GAP_PENALTY == -2 and SAME == 5 and DIFF == -5:
+            """ Given """
+            sequence1 = "GCT"
+            sequence2 = "GAAT"
 
-    """Works when config file is set according to given value, otherwise make changes in example according to config file"""
-    def setUp(self):
-        self.sequence1 = "SUM"
-        self.sequence2 = "SAM"
-        self.GAP_PENALTY = -2
-        self.SAME = -5
-        self.DIFF = 5
-        self.MAX_SEQ_LENGTH = 8
-        self.MAX_NUMBER_PATHS = 1
-        self.scoreMatrix = [[0, -2, -4, -6],
-                            [-2, 5, 3, 1],
-                            [-4, 3, 1, -1],
-                            [-6, 1, -1, 6]]
+            """ When"""
+            actual_matrix, actual_alignment_seq1, actual_alignment_seq2, match = align_sequences(sequence1, sequence2)
+            actual_score = actual_matrix[len(sequence1)][len(sequence2)]
+            expected_matrix = [[0, -2, -4, -6, -8],
+                               [-2, 5, 3, 1, -1],
+                               [-4, 3, 1, -1, -3],
+                               [-6, 1, -1, -3, 4]]
+            expected_score = 4
+            expected_alignment_seq1 = ["GC--T", "G-C-T"]
+            expected_alignment_seq2 = ["G-AAT", "GA-AT"]
 
-    def testSequenceLength(self):
-        self.assertLessEqual(len(self.sequence1), self.MAX_SEQ_LENGTH, "Sequence1 is too long.")
-        self.assertLessEqual(len(self.sequence2), self.MAX_SEQ_LENGTH, "Sequence2 is too long")
+            """ Then """
+            for i in range(0, len(sequence1)):
+                for j in range(0, len(sequence2)):
+                    self.assertEqual(actual_matrix[i][j],
+                                     expected_matrix[i][j],
+                                     "Should return correct score matrix.")
 
-    def testScoreMatrix(self):
-        expectedMatrix = self.scoreMatrix
-
-        resultMatrix = buildMatrix(self.sequence1, self.sequence2)
-
-        for i in range(0, len(self.sequence2)):
-            for j in range(0, len(self.sequence1)):
-                self.assertEqual(resultMatrix[i][j],
-                                 expectedMatrix[j][i],
-                                 "Should return correct score matrix.")
-
-    def testAlign(self):
-        expectedAlign = [['S-UM', 'SU-M'], ['SA-M', 'S-AM']]
-        actualAlign = needlemanWunschAlign(self.sequence1, self.sequence2, self.scoreMatrix)
-        self.assertListEqual(actualAlign, expectedAlign, "Alignment is not proper")
-
-    def testFinalScore(self):
-        expectedScore = 6
-        match, actualScore, finalAseq1, finalAseq2 = finalize([['S-UM', 'SU-M'], ['SA-M', 'S-AM']])
-        self.assertEqual(actualScore[0], expectedScore, "Wrong Calculation and wrong Alignment")
-
-    def testMaxPaths(self):
-        match, actualScore, finalAseq1, finalAseq2 = finalize([['S-UM', 'SU-M'], ['SA-M', 'S-AM']])
-        self.assertLessEqual(len(finalAseq1), self.MAX_NUMBER_PATHS, "Too many paths available")
-
-    def testFinalPaths(self):
-        match, actualScore, finalAseq1, finalAseq2 = finalize([['S-UM', 'SU-M'], ['SA-M', 'S-AM']])
-        expectedPathSeq1 = ['S-UM']
-        expectedPathSeq2 = ['SA-M']
-
-        self.assertEqual(finalAseq1, expectedPathSeq1, "Wrong aligned sequence1")
-        self.assertEqual(finalAseq2, expectedPathSeq2, "Wrong aligned sequence2")
+            self.assertEqual(actual_score, expected_score, "Wrong Score")
+            self.assertListEqual(actual_alignment_seq1, expected_alignment_seq1, "Alignment1 is not proper")
+            self.assertListEqual(actual_alignment_seq2, expected_alignment_seq2, "Alignment2 is not proper")
+        else:
+            stderr.write("\nSet config variables MAX_NUMBER_PATHS = 2, GAP_PENALTY = -2, SAME = 5, DIFF = -5\n\n")
+            exit(1)
 
 
 if __name__ == '__main__':
