b7738bf658be9d46863d99a25870f022fc46b260
Ami Shah <shahami.406@gmail.com>
2019-11-23 23:57:59 +0100

    Update NeedlemanWunsch.py
    made simple and clean code.
    code gives all the possible alignments.
    in old file there were so many lines to align the sequences. Now I have used recursive function (traceback()) for 3 different directions - up, diagonal and left.

--- NeedlemanWunsch.py
+++ NeedlemanWunsch.py
@@ -1,88 +1,91 @@
 from config import *
-import Alignment
+import re
 from sys import *
 from numpy import *
 import argparse as ap
 
-###############################################################################
-# finalize():                                                                 #
-#   Match symbols from both sequences, compute score and gives best alignment #
-###############################################################################
+opfile = ""
+cnt = 1
+aseq1, match_sym, aseq2 = [], [], []
 
-def finalize(sequences):
-    # # sequences contains tuples of form [(possibleAlignmentSeqs1),(possibleAlignmentSeqs2)]
-
-    alignedSeq1 = sequences[0]  # sequences[0] has all possible alignments of sequence1
-    alignedSeq2 = sequences[1]  # sequences[1] has all possible alignments of sequence2
-    sym=[]
-    scoreArray = []
-
+def traceback(i, j, left_path, up_path, diagonal_path, sequence1, sequence2, aligned_seq1, aligned_seq2):
     """
-    Fetch every sequence one by one and match symbols of both sequences with '|' 
-    if match found put '|' otherwise put ' ' 
-    if match found SAME points added, if not found then DIFF points added to score 
-    if gap is there then add GAP_PENALTY to score 
+    traceback whole matrix from bottom-right element to top-left element
+    to get all possible alignments
+    :param i: row index
+    :param j: column index
+    :param left_path: matrix that directs to left path
+    :param up_path: matrix that directs to up path
+    :param diagonal_path: matrix that directs to diagonal path
+    :param sequence1: sequence 1
+    :param sequence2: sequence 2
+    :param aligned_seq1: calculated alignment for sequence 1
+    :param aligned_seq2: calculated alignment for sequence 2
+    """
+    match_symbol = []
+    global cnt
+    if cnt <= MAX_NUMBER_PATHS:
+        if i == j == 0:
+            aligned_seq1.append(sequence1[i])
+            aligned_seq2.append(sequence2[j])
+            aligned_seq1, aligned_seq2 = aligned_seq1[::-1], aligned_seq2[::-1]
+            for symbol in range(len(aligned_seq1)):
+                match_symbol.append('|') if aligned_seq1[symbol] == aligned_seq2[symbol] else match_symbol.append(' ')
+            as1, match, as2 = ''.join(aligned_seq1), ''.join(match_symbol), ''.join(aligned_seq2)
+            cnt += 1
+            global aseq1, aseq2, match_sym
+            aseq1.append(as1)
+            aseq2.append(as2)
+            match_sym.append(match)
+        else:
+            if diagonal_path[i, j] == 1:
+                temp_seq1,temp_seq2 = aligned_seq1.copy(), aligned_seq2.copy()
+                temp_seq1.append(sequence1[i])
+                temp_seq2.append(sequence2[j])
+                traceback(i - 1, j - 1, left_path, up_path, diagonal_path, sequence1, sequence2, temp_seq1, temp_seq2)
+            if left_path[i, j] == 1:
+                temp_seq1,temp_seq2 = aligned_seq1.copy(), aligned_seq2.copy()
+                temp_seq1.append('-')
+                temp_seq2.append(sequence2[j])
+                traceback(i, j - 1, left_path, up_path, diagonal_path, sequence1, sequence2, temp_seq1, temp_seq2)
+            if up_path[i, j] == 1:
+                temp_seq1,temp_seq2 = aligned_seq1.copy(), aligned_seq2.copy()
+                temp_seq1.append(sequence1[i])
+                temp_seq2.append('-')
+                traceback(i - 1, j, left_path, up_path, diagonal_path, sequence1, sequence2, temp_seq1, temp_seq2)
+
+def align_sequences(sequence1, sequence2):
+    """
+    :param sequence1: sequence 1
+    :param sequence2: sequence 2
+    :return: scoring_matrix, alignments for both sequences and indicates matching elements from both sequences
     """
 
-    for x in range(0, len(alignedSeq1)):
-        alignedSequence1 = alignedSeq1[x]
-        alignedSequence2 = alignedSeq2[x]
-        score = 0
-        symbol = ""
-
-        for i in range(0, len(alignedSequence1)):
-            if alignedSequence1[i] == alignedSequence2[i]:
-                symbol = symbol + '|'
-                score += (SAME if alignedSequence1[i] == alignedSequence2[i] else DIFF)
-            elif alignedSequence1[i] != alignedSequence2[i] and alignedSequence1[i] != '-' and alignedSequence2[i] != '-':
-                score += (SAME if alignedSequence1[i] == alignedSequence2[i] else DIFF)
-                symbol += ' '
-            elif alignedSequence1[i] == '-' or alignedSequence2[i] == '-':
-                symbol += ' '
-                score += GAP_PENALTY
-        sym.append(symbol)
-        scoreArray.append(score)
-
-    while len(alignedSeq1) > MAX_NUMBER_PATHS:
-        alignedSeq1.pop()
-        alignedSeq2.pop()
-        scoreArray.pop()
-        sym.pop()
-    # for i in range(0, len(alignedSeq1)):
-    #     print("\n\n")
-    #     print(alignedSeq1[i])
-    #     print(sym[i])
-    #     print(alignedSeq2[i])
-    #     print("Score: ",scoreArray[i])
-
-    return sym, scoreArray, alignedSeq1, alignedSeq2
-
-#####################################################################
-# buildMatrix(): Build Matrix, Compute Matrix Elements, Fill Matrix #
-#####################################################################
-def buildMatrix(sequence1, sequence2):
-    # rows, cols variable to set size of matrix according to length of sequence
-    rows, cols = len(sequence2) + 1, len(sequence1) + 1
-    scoringMatrix = zeros((rows,cols),int)
+    scoring_matrix = zeros((len(sequence1) + 1, len(sequence2) + 1), int)
+    left_path, up_path, diagonal_path = (zeros(shape=(len(sequence1), len(sequence2))) for _ in range(3))
 
     """ compute matrix elements and fill into matrix"""
-    scoringMatrix[0][0] = 0  # Set 0th row 0th column element to zero
-    for i in range(1,rows):
-        # Fill 0th column by adding Gap penalty to each cell
-        scoringMatrix[i][0] = scoringMatrix[i-1][0] + GAP_PENALTY
-    for j in range(1,cols):
-        # Fill 0th row by adding Gap penalty to each cell
-        scoringMatrix[0][j] = scoringMatrix[0][j-1] + GAP_PENALTY
-    for i in range(1, rows):
-        for j in range(1, cols):
-            matrixValues = [scoringMatrix[i - 1][j] + GAP_PENALTY,  # Value derived from Up cell
-                            scoringMatrix[i][j - 1] + GAP_PENALTY,  # Value derived from Left cell
-                            scoringMatrix[i-1][j-1] + (SAME if sequence1[j - 1] == sequence2[i - 1] else DIFF)
-                            # Value derived from DIAGONAL cell
-                            ]
-            scoringMatrix[i][j] = max(matrixValues)  # Take maximum value among three (Up, Left, Diagonal)
-
-    return scoringMatrix
+    scoring_matrix[0][0] = 0  # Set 0th row 0th column element to zero
+    for i in range(1, len(sequence1) + 1):
+        scoring_matrix[i][0] = scoring_matrix[i - 1][0] + GAP_PENALTY  # Fill 0th column by adding Gap penalty to each cell
+    for j in range(1, len(sequence2) + 1):
+        scoring_matrix[0][j] = scoring_matrix[0][j - 1] + GAP_PENALTY  # Fill 0th row by adding Gap penalty to each cell
+    for i in range(1, len(sequence1) + 1):
+        for j in range(1, len(sequence2) + 1):
+            value_derived_from_up = scoring_matrix[i - 1][j] + GAP_PENALTY
+            value_derived_from_left = scoring_matrix[i][j - 1] + GAP_PENALTY
+            value_derived_from_diagonal = \
+                scoring_matrix[i - 1][j - 1] + (SAME if sequence1[i - 1] == sequence2[j - 1] else DIFF)
+            scoring_matrix[i][j] = max(value_derived_from_up, value_derived_from_left, value_derived_from_diagonal)
+            if scoring_matrix[i][j] == value_derived_from_left:
+                left_path[i - 1, j - 1] = 1  # denote left path matri
+            if scoring_matrix[i][j] == value_derived_from_up:
+                up_path[i - 1, j - 1] = 1  # denote up path matrix
+            if scoring_matrix[i][j] == value_derived_from_diagonal:
+                diagonal_path[i - 1, j - 1] = 1  # denote diagonal path matrix
+    traceback(len(sequence1) - 1, len(sequence2) - 1, left_path, up_path, diagonal_path, sequence1, sequence2, [], [])
+    global aseq1, aseq2, match_sym
+    return scoring_matrix, aseq1, aseq2, match_sym
 
 if __name__ == "__main__":
     """ Values taken from Command Line Arguments """
@@ -117,25 +120,23 @@ if __name__ == "__main__":
         else:
             DIFF = int(args.mismatch)
 
-    """
-    Read FASTA file and obtain both sequences
-    Create output file if not exist
-    """
+    s1_fasta = open(args.sequence1, "r")
+    s2_fasta = open(args.sequence2, "r")
+    opfile = open(args.outputFile, "w+")
 
-    s1Fasta = open(args.sequence1, "r")
-    s2Fasta = open(args.sequence2, "r")
-    outputFile = open(args.outputFile, "w+")
+    sequence1,sequence2 = s1_fasta.read(),s2_fasta.read()
 
-    sequence1 = s1Fasta.read().replace("\n", "")
-    sequence2 = s2Fasta.read().replace("\n", "")
-    sequence1 = sequence1.replace(" ","")
-    sequence2 = sequence2.replace(" ","")
-    sequence1 = sequence1.upper()
-    sequence2 = sequence2.upper()
+    if sequence1[0] == '>':
+        sequence1 = sequence1.partition("\n")
+        sequence1 = sequence1[2].replace("\n", "").replace(" ", "").upper()
+    else:
+        sequence1 = sequence1.replace("\n", "").replace(" ", "").upper()
 
-    ##############################################################
-    # Checking MAX_NUM_PATHS and Whether Sequences are Too Large #
-    ##############################################################
+    if sequence2[0] == '>':
+        sequence2 = sequence2.partition("\n")
+        sequence2 = sequence2[2].replace("\n", "").replace(" ", "").upper()
+    else:
+        sequence2 = sequence2.replace("\n", "").replace(" ", "").upper()
 
     if (len(sequence1) > MAX_SEQ_LENGTH) and (len(sequence2) > MAX_SEQ_LENGTH):
         stderr.write("Length of both sequences are too long")
@@ -147,20 +148,13 @@ if __name__ == "__main__":
         stderr.write("Length of sequence2 is too long")
         exit(1)
     else:
-        scoringMatrix = buildMatrix(sequence1, sequence2)
-        print("Left Side Sequence: ", sequence2)
-        print("Up side Sequence: ", sequence1)
-        print("Maximum Paths: ", MAX_NUMBER_PATHS)
-        print("\nScoring Matrix:\n", scoringMatrix)
-
-        sequences = Alignment.needlemanWunschAlign(sequence1, sequence2, scoringMatrix)
-        match, score, alignedSequence1, alignedSequence2 = finalize(sequences)
-
-        """Writing final result in output file"""
-
-        for i in range(0, len(match)):
-            print("Alignment(Path) ", i+1, ":\n", alignedSequence1[i],"\n",match[i],"\n",alignedSequence2[i],"\nScore: ",score[i],"\n\n")
-            outputFile.write("Alignment %s:\n%s\n%s\n%s\nScore: %s\n\n" % ((i + 1), alignedSequence1[i], match[i], alignedSequence2[i], score[i]))
-
-        outputFile.close()
-
+        if bool(re.match('^[GCAUT]+$', sequence1)) and bool(re.match('^[GCAUT]+$', sequence2)):
+            scoring_matrix, as1, as2, match = align_sequences(sequence1, sequence2)
+            print("\nScoring Matrix: \n", scoring_matrix, "\n\nScore: ", scoring_matrix[len(sequence1)][len(sequence2)])
+            for i in range(0, len(as1)):
+                opfile.write("Alignment: %s\n%s\n%s\n%s\n\n" % (i+1, as1[i], match[i], as2[i]))
+                print("\nAlignment ", i+1, ":\n", as1[i], "\n", match[i], "\n", as2[i])
+            opfile.close()
+        else:
+            print("Only DNA or mRNA sequence is allowed")
+            exit(1)
